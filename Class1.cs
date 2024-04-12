c6e869f77ff52889a59b5d22aaaea1acde9f404d
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-04-12 11:53:00 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uppgift_oop4
+{
+    internal class namn
+    {
+        private string helnamn;
+        private string förnamn;
+        private string efternamn;
+        private double höjd;
+        private double vikt;
+
+        public string Förnamm
+        {
+            get { return förnamn; }
+
+            set { förnamn = value; }
+        }
+
+        public string Efternamn
+        {
+            get { return efternamn; }
+
+            set { efternamn = value; }
+        }
+
+        public double Höjd
+        {
+            get { return höjd; }
+
+            set { höjd = value; }
+
+        }
+
+        public double Vikt
+        {
+            get { return vikt; }
+
+            set { vikt = value; }
+
+        }
+
+        public double Bmi()
+        {
+            double bmi = vikt / (höjd * höjd);
+            return bmi;
+        }
+        public void Namn()
+        {
+            helnamn = förnamn + " " + efternamn;
+            Console.WriteLine("Hej " + helnamn);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace inlämningsuppgift._4_oop 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            namn p = new namn();
+            Console.WriteLine("Förnamn?");
+            p.Förnamm = Console.ReadLine();
+            Console.WriteLine("Efternamn?");
+            p.Efternamn = Console.ReadLine();
+            Console.WriteLine("");
+            p.Namn();
+
+            bool myndig = false;
+            Console.WriteLine("");
+            Console.WriteLine("Vilket år är du född?");
+            int år = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken månad är du född?");
+            int månad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Vilken dag är du född?");
+            int dag = int.Parse(Console.ReadLine());
+            DateTime nu = DateTime.Today;
+            DateTime årsedan = DateTime.Today.AddYears(-18);
+            DateTime person = new DateTime(år, månad, dag);
+
+            if (person < årsedan)
+            {
+                myndig = true;
+            }
+
+            Console.WriteLine("Din myndighetsstatus är " + myndig);
+            Console.WriteLine("");
+            Console.WriteLine("Hur mycket väger du i kg?");
+            p.Vikt = double.Parse(Console.ReadLine());
+            Console.WriteLine("Hur lång är du i m?");
+            p.Höjd = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Ditt Bmivärde är " + p.Bmi());
+
+        }
+    }
+}
